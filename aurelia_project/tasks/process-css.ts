e3a6993a91466cf8ce16fb516123dfd2e017f71e
Simon Farrugia <Simon Farrugia>
2016-12-04 22:16:35 +0100

    Added PostCSS

--- aurelia_project/tasks/process-css.ts
+++ aurelia_project/tasks/process-css.ts
@@ -2,18 +2,25 @@ import * as gulp from 'gulp';
 import * as changedInPlace from 'gulp-changed-in-place';
 import * as sourcemaps from 'gulp-sourcemaps';
 import * as postcss from 'gulp-postcss';
-import * as autoprefixer from 'autoprefixer';
+// import * as autoprefixer from 'autoprefixer';
+import * as cssnext from 'postcss-cssnext';
+import * as use from 'postcss-use';
 import * as project from '../aurelia.json';
 import {build} from 'aurelia-cli';
 
 export default function processCSS() {
+
+  //We are applying cssnext on all files to polyfill future css syntax by default
+  //And using postcss-use we also allow individual css files to declare their own css processors dependecies (example: SCSS)
   let processors = [
-    autoprefixer({browsers: ['last 1 version']})
+    //autoprefixer({browsers: ['last 1 version']}),
+    cssnext(), //includes autoprefixer
+    use({ resolveFromFile: true, modules: '*' })
   ];
 
   return gulp.src(project.cssProcessor.source)
     .pipe(changedInPlace({firstPass:true}))
-    //.pipe(sourcemaps.init())
+    .pipe(sourcemaps.init())
     .pipe(postcss(processors))
     .pipe(build.bundle());
 };
--- wwwroot/scripts/app-bundle.js
+++ wwwroot/scripts/app-bundle.js
@@ -57,6 +57,23 @@ define('resources/index',["require", "exports"], function (require, exports) {
     exports.configure = configure;
 });
 
+define('layouts/document/document',["require", "exports"], function (require, exports) {
+    "use strict";
+    var Document = (function () {
+        function Document() {
+        }
+        Document.prototype.activate = function (settings, nav) {
+            this.settings = settings;
+        };
+        return Document;
+    }());
+    exports.Document = Document;
+});
+
+
+
+define("layouts/search-layout/search-layout", [],function(){});
+
 define('pages/about/about',["require", "exports"], function (require, exports) {
     "use strict";
     var About = (function () {
@@ -96,23 +113,6 @@ define('pages/daterangepicker/daterangepicker',["require", "exports"], function 
     exports.Daterangepicker = Daterangepicker;
 });
 
-define('layouts/document/document',["require", "exports"], function (require, exports) {
-    "use strict";
-    var Document = (function () {
-        function Document() {
-        }
-        Document.prototype.activate = function (settings, nav) {
-            this.settings = settings;
-        };
-        return Document;
-    }());
-    exports.Document = Document;
-});
-
-
-
-define("layouts/search-layout/search-layout", [],function(){});
-
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -150,13 +150,13 @@ define('resources/elements/app-container/app-container',["require", "exports"], 
 
 define('text!app.html', ['module'], function(module) { module.exports = "<template>\n\n  <require from=\"./resources/elements/app-container/app-container\"></require>\n  \n\n  <h1>${message}</h1>\n  <app-container>\n        <p>olllllllllllllo</p>\n        <router-view></router-view>\n  </app-container>\n\n</template>\n"; });
 define('text!layouts/search-layout/search-layout.css', ['module'], function(module) { module.exports = ""; });
-define('text!test.html', ['module'], function(module) { module.exports = ""; });
-define('text!pages/about/about.css', ['module'], function(module) { module.exports = ""; });
 define('text!layouts/document/document.html', ['module'], function(module) { module.exports = "<template>\r\n    \r\n    <main>\r\n        <slot></slot>\r\n    </main>\r\n\r\n</template>"; });
+define('text!pages/about/about.css', ['module'], function(module) { module.exports = ""; });
 define('text!layouts/search-layout/search-layout.html', ['module'], function(module) { module.exports = ""; });
-define('text!pages/about/about.html', ['module'], function(module) { module.exports = "<template>\r\n    <p>juhuuuuuu</p>\r\n</template>"; });
+define('text!resources/elements/app-container/app-container.css', ['module'], function(module) { module.exports = "\r\n.app-frame {\r\n\r\n    position: relative;\r\n    width: 100%;\r\n    height: 100%;\r\n}\r\n\r\n.app-frame__side-bar {\r\n\r\n    position: absolute;\r\n    top: 0;\r\n    bottom: 0;\r\n    left: 0;\r\n    width: 0px;\r\n    will-change: width\r\n}\r\n\r\n@media(min-width: 544px) {\r\n\r\n    .app-frame__side-bar {\r\n\r\n        width: 50px;\r\n    }\r\n    }\r\n\r\n.app-frame--side-bar-open .app-frame__side-bar {\r\n\r\n    width: 80%;\r\n    min-width: 200px;\r\n    max-width: 100%\r\n}\r\n\r\n@media(min-width: 544px) {\r\n\r\n    .app-frame--side-bar-open .app-frame__side-bar {\r\n\r\n        width: 200px;\r\n    }\r\n    }\r\n\r\n.app-frame__page {\r\n\r\n    position: absolute;\r\n    top: 0;\r\n    bottom: 0;\r\n    left: 0;\r\n    right: 0;\r\n}\r\n\r\n.app-frame__mobile-nav {\r\n\r\n    position: absolute;\r\n    top: 50px;\r\n    bottom: 0;\r\n    left: 0;\r\n    right: 0\r\n}\r\n\r\n@media(min-width: 544px) {\r\n\r\n    .app-frame__mobile-nav {\r\n\r\n        top: 0px;\r\n    }\r\n    }"; });
 define('text!pages/components/components.html', ['module'], function(module) { module.exports = "\r\n<template>\r\n     ullala\r\n    <aside>\r\n        <ul>\r\n            <li></li>\r\n        </ul>\r\n    </aside>\r\n\r\n    <router-view></router-view>\r\n\r\n</template>"; });
+define('text!pages/about/about.html', ['module'], function(module) { module.exports = "<template>\r\n    <p>juhuuuuuu</p>\r\n</template>"; });
 define('text!pages/daterangepicker/daterangepicker.html', ['module'], function(module) { module.exports = "\r\n<template>\r\n    <require from=\"resources/elements/bs-daterangepicker\"></require>\r\n\r\n    <h1>Daterangepicker Page</h1>\r\n    <bs-daterangepicker value=\"hello\"></bs-daterangepicker>\r\n\r\n</template>"; });
 define('text!resources/elements/bs-daterangepicker.html', ['module'], function(module) { module.exports = "<template>\n  <h1>hey</h1>\n</template>"; });
-define('text!resources/elements/app-container/app-container.html', ['module'], function(module) { module.exports = "<template>\r\n\r\n    <aside></aside>\r\n    <h1>hola</h1>\r\n    <div class=\"page\">\r\n        <slot></slot>\r\n    </div>\r\n\r\n</template>"; });
+define('text!resources/elements/app-container/app-container.html', ['module'], function(module) { module.exports = "<template>\r\n\r\n    <div class=\"app-frame app-frame--open\">\r\n        \r\n        <div class=\"app-frame__side-nav\">\r\n\r\n            <div class=\"side-nav\">\r\n\r\n                <div class=\"side-nav__top\">\r\n                    <div class=\"logo\"></div>\r\n                    <div class=\"back-button\"></div>\r\n                    <div class=\"nav-links\">\r\n                        <a class=\"\">Home</a>\r\n                        <a>Favourites</a>\r\n                    </div>\r\n                </div>\r\n                \r\n                <div class=\"side-nav__bottom\">\r\n                    <div class=\"authentication\"></div>\r\n                    <div class=\"side-nav-toggle\"></div>\r\n                </div>\r\n\r\n            </div>\r\n\r\n        </div>\r\n\r\n        <div class=\"app-frame__page\">\r\n            \r\n            <div class=\"page\">\r\n                <div class=\"page__overlay\"></div>\r\n                <slot></slot>\r\n            </div>\r\n\r\n        </div>\r\n\r\n        <div class=\"app-frame__mobile-nav\">\r\n            \r\n            <div class=\"mobile-nav__left\">\r\n                <div class=\"btn-icon btn-icon-side-nav-toggle\"></div>\r\n                <a class=\"btn-icon btn-icon-search\"></div>\r\n                <a class=\"btn-icon btn-icon-back\"></div>\r\n            </div>\r\n\r\n            <div class=\"mobile-nav__right\">\r\n                <div class=\"notifications\"></div>\r\n            </div>\r\n\r\n        </div>\r\n\r\n    </div>\r\n\r\n</template>"; });
 //# sourceMappingURL=app-bundle.js.map
