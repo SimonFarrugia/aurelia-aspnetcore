8204571e7ea76d7b66d0724d565947fe5dadeb07
Simon Farrugia <Simon Farrugia>
2017-01-15 20:47:21 +0100

    Appdrawer update

--- src/resources/elements/app-frame/app-drawer/app-drawer-toggle/app-drawer-toggle.ts
+++ src/resources/elements/app-frame/app-drawer/app-drawer-toggle/app-drawer-toggle.ts
@@ -1,5 +1,5 @@
 import {inject, bindable} from "aurelia-framework";
-import s from "svg.js";
+import * as SVG from "svg.js";
 
 @inject(Element)
 export class AppDrawerToggle {
@@ -10,14 +10,27 @@ export class AppDrawerToggle {
 
     openChanged(newValue: boolean, oldValue: Boolean) {
         
-        let svgElement: svgjs.Element = s.select("svg", this.element as HTMLElement)
+        //let svg:svgjs.Library = (SVG.default || SVG) as svgjs.Library;
+
+        let svgElement: svgjs.Element = SVG.select("svg path", this.element as HTMLElement)
 
         if(newValue) {
-            
+            svgElement.attr({ d: 'M4 0l-4 4 4 4 1.5-1.5-2.5-2.5 2.5-2.5-1.5-1.5z' });
         }
         else {
-
+            svgElement.attr({ d: 'M1.5 0l-1.5 1.5 2.5 2.5-2.5 2.5 1.5 1.5 4-4-4-4z' });
         }
+    }
+
+    created() {
+
+        //let svg:svgjs.Library = (SVG.default || SVG) as svgjs.Library;
+
+        let svgElement = SVG(this.element as HTMLElement);
+        svgElement.viewbox(new SVG.ViewBox([0, 0, 8, 8]));
+        svgElement.path("M1.5 0l-1.5 1.5 2.5 2.5-2.5 2.5 1.5 1.5 4-4-4-4z");
+        svgElement.transform("translate(1)");
+
     }
 
 }
